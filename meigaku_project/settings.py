c3982d90ae5fa6ba82d048274081c26d9f5f7e16
mamoru9045 <mamorubasebaii9045@gmail.com>
2022-02-01 11:49:24 +0900

    staticfiles

--- meigaku_project/settings.py
+++ meigaku_project/settings.py
@@ -143,7 +143,7 @@ MEDIA_URL='/media/'
 # STATIC_URL = '/static/'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
